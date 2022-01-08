d007873bbc6fb19a3bf411e483c5e91fbc60452e
Christopher Stock <christopher.stock@mayflower.de>
2022-01-08 11:59:24 +0100

    Machine image type from 't3.micro' to 't2.micro'.

--- terraform/ec2_instance.tf
+++ terraform/ec2_instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "workshop_ec2_instance" {
     ami                  = "ami-67cbd003"
-    instance_type        = "t3.micro"
+    instance_type        = "t2.micro"
     security_groups      = ["${aws_security_group.workshop_ecs_security_group.name}"]
     iam_instance_profile = "${aws_iam_instance_profile.workshop_iam_instance_profile.name}"
     user_data            = <<EOF
